adfde0505c7585826a8222f572e6b39f2ff609a8
Vence <vence.lin@ico.com.hk>
2016-08-11 18:38:12 +0800

    initial commit

---
+++ memutil.go
@@ -0,0 +1,49 @@
+// memutil v0.1
+// @description A util library for allocating native memory
+// @authors     Vence Lin(vence722@gmail.com)
+package memutil
+
+//#include<stdlib.h>
+import "C"
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func NativeNew(proto interface{}) interface{} {
+	p := C.malloc(C.size_t(reflect.TypeOf(proto).Size()))
+	if p == nil {
+		panic("failed to call C.malloc()")
+	}
+	return reflect.NewAt(reflect.TypeOf(proto), p).Interface()
+}
+
+func NativeDelete(ptr interface{}) {
+	C.free(unsafe.Pointer(reflect.ValueOf(ptr).Pointer()))
+}
+
+type NativeBuffer []byte
+
+func (this *NativeBuffer) init(addr unsafe.Pointer, size int) {
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(this))
+	sh.Data = uintptr(addr)
+	sh.Len = size
+	sh.Cap = size
+}
+
+func (this *NativeBuffer) Ptr() uintptr {
+	ptrH := (*reflect.SliceHeader)(unsafe.Pointer(this))
+	return ptrH.Data
+}
+
+func NativeAllocateBuffer(size int) NativeBuffer {
+	buffer := NativeBuffer{}
+	addr := C.malloc(C.size_t(size))
+	buffer.init(addr, size)
+	return buffer
+}
+
+func NativeFreeBuffer(buffer NativeBuffer) {
+	C.free(unsafe.Pointer(buffer.Ptr()))
+}
---
+++ memutil_test.go
@@ -0,0 +1,36 @@
+package memutil
+
+import (
+	"testing"
+)
+
+type TestType struct {
+	TestField  int
+	TestString string
+}
+
+var TestTypeProto TestType
+
+func TestNativeNewAndDelete(t *testing.T) {
+	var list []*TestType
+	for i := 0; i < 100; i++ {
+		testType := NativeNew(TestTypeProto).(*TestType)
+		testType.TestField = 3
+		testType.TestString = "sdfsdf"
+		t.Log(testType.TestField, testType.TestString)
+		list = append(list, testType)
+	}
+	for i := 0; i < 100; i++ {
+		NativeDelete(list[i])
+	}
+}
+
+func TestNativeBuffer(t *testing.T) {
+	buffer := NativeAllocateBuffer(1024)
+	t.Log(len(buffer))
+	buffer[1023] = 30
+	t.Log(buffer[1023])
+	t.Log("ptr:", buffer.Ptr())
+	NativeFreeBuffer(buffer)
+	t.Log(buffer[1023])
+}
